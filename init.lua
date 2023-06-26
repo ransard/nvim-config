0c08d74ff21fe1041696ee3c1253f91ddf84ace4
Mattias Berglund <mattias.k.berglund@gmail.com>
2023-06-26 19:00:00 +0200

    formatting

--- init.lua
+++ init.lua
@@ -1,15 +1,21 @@
 return {
-plugins = {
-                                    { "akinsho/toggleterm.nvim", opts = { direction = "horizontal" } }
-                  },
-mappings = {
-n = {
-[",b"] = { function() require("astronvim.utils.buffer").nav(vim.v.count > 0 and vim.v.count or 1) end, desc = "previous buffer"},
-[".b"] = { function() require("astronvim.utils.buffer").nav(-(vim.v.count > 0 and vim.v.count or 1)) end, desc = "next buffer"},
-["<leader>tt"] = { function() require("toggleterm").toggle(vim.v.count) end, desc="toggle term" }
-},
-t = {
-["<Esc>"] = { "<C-\\><C-n>", desc = "escape terminal" }
-                                    }
-      }
+  plugins = {
+    { "akinsho/toggleterm.nvim", opts = { direction = "horizontal" } },
+  },
+  mappings = {
+    n = {
+      [",b"] = {
+        function() require("astronvim.utils.buffer").nav(vim.v.count > 0 and vim.v.count or 1) end,
+        desc = "previous buffer",
+      },
+      [".b"] = {
+        function() require("astronvim.utils.buffer").nav(-(vim.v.count > 0 and vim.v.count or 1)) end,
+        desc = "next buffer",
+      },
+      ["<leader>tt"] = { function() require("toggleterm").toggle(vim.v.count) end, desc = "toggle term" },
+    },
+    t = {
+      ["<Esc>"] = { "<C-\\><C-n>", desc = "escape terminal" },
+    },
+  },
 }
